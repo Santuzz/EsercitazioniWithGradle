0220ff540f0823c5e3834a1144c82807874d4958
Davide <davi9898@gmail.com>
2022-03-12 11:44:12 +0100

    Initial commit

---
+++ src/main/java/Arrays.java
@@ -0,0 +1,117 @@
+/**
+ * You can find many more here:
+ * https://www.w3resource.com/java-exercises/array/index.php
+ *
+ * Preferences -> Editor -> General -> Code folding -> One-line methods (uncheck)
+ * Code -> Folding -> Collapse All
+ * Code -> Folding -> Expand Doc Comments
+ */
+public class Arrays {
+    /**
+     * Write a function accepting a double[] and a factor f (double)
+     * returning a double[] representing the original double[] divided by f
+     */
+    public static double[] divideArray(double[] in, double f) {
+        int len = in.length;
+        double[] out = new double[len];
+        for (int i = 0; i < len; i++) {
+            out[i] = in[i] / f;
+        }
+        return out;
+    }
+
+    /**
+     * Write a function accepting two double[]
+     * returning a double[] representing the first array divided by the second array
+     * The two arrays must have the same size. Returns null otherwise.
+     */
+    public static double[] divideArrays(double[] a, double[] b) {
+        double[] out = new double[a.length];
+        for (int i = 0; i < out.length; i++) {
+            out[i] = a[i] / b[i];
+        }
+        return out;
+    }
+
+    /**
+     * Write a function accepting an int[]
+     * returning a sorted version of it (bubble sort)
+     */
+    static int[] bubbleSort(int[] v) {
+        int len = v.length;
+        boolean finito = false;
+        while (!finito) {
+            finito = true;
+            for(int i = 0; i<len-1;i++) {
+                if(v[i] > v[i + 1]){
+                    int tmp = v[i];
+                    v[i] = v[i + 1];
+                    v[i + 1] = tmp;
+                    finito = false;
+                }
+            }
+        }
+        return v;
+    }
+
+    /**
+     * Write a function accepting an int
+     * returning a long[] composed of the first n numbers of the Fibonacci series (without recursion).
+     */
+    public static long[] fibonacci(int n) {
+        long[] fibo = new long[n];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                fibo[0] = 0;
+            } else if (i == 1) {
+                fibo[1] = 1;
+            } else {
+                fibo[i] = fibo[i - 1] + fibo[i - 2];
+            }
+        }
+        return fibo;
+    }
+
+    /**
+     * Write a function accepting an int[]
+     * returning an int[] in which all the 0s have been moved to the end of an array.
+     * Maintain the relative order of the other (non-zero) elements.
+     */
+    public static int[] moveZerosEnd(int[] v) {
+        int len = v.length;
+        int s = 0, f = len - 1;
+        int[] out = new int[len];
+        for (int i : v) {
+            if (i == 0) {
+                out[f] = i;
+                f--;
+            } else {
+                out[s] = i;
+                s++;
+            }
+        }
+        return out;
+    }
+
+    /**
+     * Write a function accepting an int
+     * returning the sequence of individual digits.
+     * (e.g. 363738229 -> [3,6,3,7,3,8,2,2,9])
+     */
+    public static int[] splitter(int input) {
+        int len = 0;
+        int div = input;
+        while (div != 0) {
+            len++;
+            div /= 10;
+        }
+        int[] out = new int[len];
+        for (int i = len-1; i >= 0; i--) {
+
+            out[i] = input % 10;
+            input /= 10;
+        }
+        return out;
+    }
+
+}
---
+++ src/main/java/Strings.java
@@ -0,0 +1,133 @@
+/**
+ * You can find many more here:
+ * https://www.w3resource.com/java-exercises/string/index.php
+ *
+ * Preferences -> Editor -> General -> Code folding -> One-line methods (uncheck)
+ * Code -> Folding -> Collapse All
+ * Code -> Folding -> Expand Doc Comments
+ */
+public class Strings {
+    /**
+     * Write a function accepting a string[]
+     * returning a single String representing the concatenation of all Strings of the array
+     * see StringBuilder
+     */
+    public static String concatenate(String[] strings) {
+        StringBuilder str = new StringBuilder();
+        for (String iesim : strings) {
+            str.append(iesim);
+        }
+        return str.toString();
+    }
+
+    /**
+     * Write a function accepting a string
+     * returning the same string but reversed
+     */
+    public static String reverse(String s) {
+        StringBuilder rev = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev.append(s.charAt(i));
+        }
+        return rev.toString();
+    }
+
+    /**
+     * Write a function accepting a string
+     * returning the string without the first two characters.
+     */
+    public static String removeFirstTwoChars(String s) {
+        StringBuilder two = new StringBuilder();
+        for (int i = 2; i < s.length(); i++) {
+            two.append(s.charAt(i));
+        }
+        return two.toString();
+        // return s.substring(2);
+    }
+
+    /**
+     * Write a function accepting a string
+     * returning the string without the first two characters.
+     * Keep the first character if it is 'H' and
+     * keep the second character if it is 'e'.
+     */
+    public static String removeFirstTwoCharsIf(String s) {
+        if (s.charAt(0) == 'H' && s.charAt(1) == 'e') {
+            return s;
+        } else if (s.charAt(0) == 'H') {
+            return s.charAt(0) + s.substring(2);
+        } else if (s.charAt(1) == 'e') {
+            return s.substring(1);
+        } else
+            return s.substring(2);
+    }
+
+    /**
+     * Write a function accepting a string
+     * returning true if "good" appears at the beginning of the string
+     */
+    public static boolean goodAtTheBeginning(String s) {
+        return s.substring(0, 4).toString().equals("good");
+    }
+
+    /**
+     * Write a function accepting a string
+     * returning true if "good" appears
+     * starting at index 0 or 1 in the given string.
+     * see substring() method
+     */
+    public static boolean goodAroundTheBeginning(String s) {
+        return s.substring(0, 4).equals("good") || s.substring(1, 5).equals("good");
+    }
+
+    /**
+     * Write a function accepting a string
+     * returning the same string (trimmed of the first and last characters) if the first or last characters are equal
+     * or returning the same string (without trimming anything) otherwise
+     * see substring(), charAt() methods
+     */
+    public static String removeFirstLast(String s) {
+        if (s.charAt(0) == s.charAt(s.length() - 1)) {
+            return s.substring(1, s.length() - 1);
+        }
+        return s;
+    }
+
+    /**
+     * Write a function accepting a string
+     * returning all recurring characters contained into the string as a char[]
+     */
+    public static char[] duplicateChars(String input) {
+        char[] charazzi = new char[input.length()];
+        char[] complete;
+        int k = 0;
+        for (int j = 0; j < input.length() - 1; j++) {
+            for (int i = j - 1; i >= 0; i--) {
+                if (input.charAt(i) == input.charAt(j)) {
+                    charazzi[k] = input.charAt(i);
+                    k++;
+                }
+            }
+        }
+        complete = new char[k];
+        System.arraycopy(charazzi, 0, complete, 0, k);
+        return complete;
+    }
+
+    /**
+     * Write a function accepting a string
+     * returning true if the string is a palindrome.
+     * see StringBuilder methods
+     */
+
+    public static boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
