c1f464d2cf9c1af7ecd0f62d6621a2a817dcdce4
Davide <davi9898@gmail.com>
2022-03-18 17:21:56 +0100

    Complete Street, rectangle exercises

---
+++ src/test/java/objectoriented/anphibious/CarTest.java
@@ -0,0 +1,13 @@
+package objectoriented.anphibious;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+    @Test
+    void run() {
+        LandVehicle c = new Car();
+        assertEquals("Car running...", c.run());
+    }
+}
